ab9c1bf93dbc4f8850c0b3dfdb9df3ba747011cc
nailhas <nail.has@gmail.com>
2020-03-24 22:06:37 -0400

    Added A26_MethodsWithString_PrintUniqueChars.java

--- Replits/src/replits/A26_MethodsWithString_PrintUniqueChars.java
+++ Replits/src/replits/A26_MethodsWithString_PrintUniqueChars.java
@@ -1,5 +1,26 @@
 package replits;
 
-public class A26_MethodsWithString_PrintUniqueChars {
+import java.util.Scanner;
 
-}
+public class A26_MethodsWithString_PrintUniqueChars {
+/*The method should accept any string and return unique characters from the parameter.
+	Ex: uniqueChars("java") ==> "jav"
+		uniqueChars("mama") ==> "ma"
+		uniqueChars("spoon") ==> "spon"     */
+		
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			String str = scan.nextLine();
+			System.out.println(uniqueChars(str));
+		}
+		
+		public static String uniqueChars(String str) {
+			String unique = "";
+			for(int i=0; i<str.length(); i++) {
+				if(!unique.contains(""+str.charAt(i))) {
+					unique+=str.charAt(i);
+				}
+			}
+			return unique;
+		}
+	}
