cf1cbf7b8441582c485ed6b18861634038abeb74
nailhas <nail.has@gmail.com>
2020-04-12 19:09:50 -0400

    Added A55_MethodsWithArr_AddElement.java

---
+++ Replits/src/replits/A55_MethodsWithArr_AddElement.java
@@ -0,0 +1,36 @@
+package replits;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A55_MethodsWithArr_AddElement {
+/*Add_to_r is a method that gets an array and an int.
+The method creates a new array bigger by one then (int[] r).
+It populates the new array with the old ones(r) values
+and, finally, in the last position adds the number (int n) to it.
+		Ex: method input: add_to_r(new int{1,5,77,8} , 2)
+		outputs (int array):
+		[1, 5, 77, 8, 2]         */
+
+		public static void add_to_r(int[] r, int n) {			
+			int newSize = r.length+1;
+			int[] new_r = new int[newSize];
+			for(int i=0; i<r.length; i++) {
+				new_r[i]=r[i];
+				}
+			new_r[newSize-1]=n;
+			System.out.println(Arrays.toString(new_r));						
+		}
+		
+		public static void main(String[] args) {
+			Scanner inp = new Scanner(System.in);
+			int size = inp.nextInt();
+			int n = inp.nextInt();
+			int[] arr = new int[size];
+			for(int i=0; i<size-1; i++) {
+				arr[i]=inp.nextInt();
+			}
+			
+			add_to_r(arr, n);
+		}
+	}
